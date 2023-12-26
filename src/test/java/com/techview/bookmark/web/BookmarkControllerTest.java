4065f853d4ca2b05e280479f9302a7b13acbf935
DilendraSajini <dilendra.sajini@gmail.com>
2023-12-26 17:26:16 +0000

    Add docker plugin

--- src/test/java/com/techview/bookmark/web/BookmarkControllerTest.java
+++ src/test/java/com/techview/bookmark/web/BookmarkControllerTest.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
